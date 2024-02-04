544930a40e6a0c486e6baae6147bad7496d229f3
Muhammad Hasham <hasham25525@gmail.com>
2024-02-05 01:29:22 +0500

    working on cart page

---
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import classes from './index.module.scss'
+
+const CartItem = () => {
+  return (
+    <li className={classes.item}>
+        <h6>Title</h6>
+    </li>
+  )
+}
+
+export default CartItem
--- src/app/_heros/Product/index.tsx
+++ src/app/_heros/Product/index.tsx
@@ -23,9 +23,18 @@ export const ProductHero: React.FC<{
   } = product
 
   return (
-    
-      <Gutter className={classes.productHero}>
-        <div className={classes.content}>
+    <Gutter className={classes.productHero}>
+      <div className={classes.mediaWrapper}>
+        {!metaImage && <div className={classes.placeholder}>No image</div>}
+        {metaImage && typeof metaImage !== 'string' && (
+          <Media imgClassName={classes.image} resource={metaImage} fill />
+        )}
+      </div>
+
+      <div className={classes.details}>
+        <h3 className={classes.title}>{title}</h3>
+
+        <div className={classes.categoryWrapper}>
           <div className={classes.categories}>
             {categories?.map((category, index) => {
               if (typeof category === 'object' && category !== null) {
@@ -36,40 +45,27 @@ export const ProductHero: React.FC<{
                 const isLast = index === categories.length - 1
 
                 return (
-                  <Fragment key={index}>
+                  <p className={classes.category} key={index}>
                     {titleToUse}
                     {!isLast && <Fragment>, &nbsp;</Fragment>}
-                  </Fragment>
+                    <span className={classes.separator}>|</span>
+                  </p>
                 )
               }
 
               return null
             })}
           </div>
-          <h1 className={classes.title}>{title}</h1>
-          <div>
-            <p className={classes.description}>
-              {`${description ? `${description} ` : ''}To edit this product, `}
-              <Link href={`${process.env.NEXT_PUBLIC_SERVER_URL}/admin/collections/products/${id}`}>
-                navigate to the admin dashboard
-              </Link>
-              {'.'}
-            </p>
-          </div>
-          <Price product={product} button={false} />
-          <AddToCartButton product={product} className={classes.addToCartButton} />
+          <p className={classes.stock}>In Stock</p>
         </div>
-        <div className={classes.media}>
-          <div className={classes.mediaWrapper}>
-            {!metaImage && <div className={classes.placeholder}>No image</div>}
-            {metaImage && typeof metaImage !== 'string' && (
-              <Media imgClassName={classes.image} resource={metaImage} fill />
-            )}
-          </div>
-          {metaImage && typeof metaImage !== 'string' && metaImage?.caption && (
-            <RichText content={metaImage.caption} className={classes.caption} />
-          )}
+
+        <Price product={product} button={false} />
+        <div className={classes.description}>
+          <h6>Description</h6>
+          <p>{description}</p>
         </div>
-      </Gutter>
+        <AddToCartButton product={product} className={classes.addToCartButton} />
+      </div>
+    </Gutter>
   )
 }
